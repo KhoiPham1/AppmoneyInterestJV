21248f387b125bc1b54149b0b39be5c8ed5e6a68
Khoi Pham <khoi171195@gmail.com>
2018-10-27 17:09:05 +0700

    firstcommit

---
+++ src/AppMoneyInterest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AppMoneyInterest {
+    public static void main(String[] args) {
+        double money;
+        int month;
+        double interest_rate;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter investment amount: ");
+        money = input.nextDouble();
+        System.out.println("Enter number of months: ");
+        month = input.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interest_rate = input.nextDouble();
+        double total_interest = money*(interest_rate/100)/12*month;
+        System.out.println("Total of interest: " + total_interest);
+
+    }
+
+}
